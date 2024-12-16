acc8a652c43d812f97c8fb7f296a420ff9869d8c
Arckadiy Shchukin <schukark2004@gmail.com>
2024-12-16 09:28:50 +0300

    stage: (day12) part2 attempt

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::utils::input;
 
@@ -16,10 +16,10 @@ const MAT: [(isize, isize); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
 
 fn dfs(
     grid: &[Vec<char>],
-    visited: &mut HashSet<(usize, usize)>,
-    cur_area: &mut usize,
-    cur_perimeter: &mut usize,
-    current: (usize, usize),
+    visited: &mut HashSet<(isize, isize)>,
+    cur_area: &mut isize,
+    cur_perimeter: &mut isize,
+    current: (isize, isize),
 ) {
     // println!("Visiting {:#?}", current);
     visited.insert(current);
@@ -27,8 +27,8 @@ fn dfs(
     *cur_perimeter += 4;
 
     for (dx, dy) in MAT {
-        let new_x = current.0 as isize + dx;
-        let new_y = current.1 as isize + dy;
+        let new_x = current.0 + dx;
+        let new_y = current.1 + dy;
 
         if new_x < 0 || new_x >= grid.len() as isize || new_y < 0 || new_y >= grid[0].len() as isize
         {
@@ -37,17 +37,23 @@ fn dfs(
 
         let (new_x, new_y) = (new_x as usize, new_y as usize);
 
-        if grid[new_x][new_y] != grid[current.0][current.1] {
+        if grid[new_x][new_y] != grid[current.0 as usize][current.1 as usize] {
             continue;
         }
 
         *cur_perimeter -= 1;
 
-        if visited.contains(&(new_x, new_y)) {
+        if visited.contains(&(new_x as isize, new_y as isize)) {
             continue;
         }
 
-        dfs(grid, visited, cur_area, cur_perimeter, (new_x, new_y));
+        dfs(
+            grid,
+            visited,
+            cur_area,
+            cur_perimeter,
+            (new_x as isize, new_y as isize),
+        );
     }
 }
 
@@ -62,7 +68,7 @@ fn part1(input: &str) {
 
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            if visited.contains(&(i, j)) {
+            if visited.contains(&(i as isize, j as isize)) {
                 continue;
             }
 
@@ -74,7 +80,7 @@ fn part1(input: &str) {
                 &mut visited,
                 &mut cur_area,
                 &mut cur_perimeter,
-                (i, j),
+                (i as isize, j as isize),
             );
 
             result += cur_area * cur_perimeter;
@@ -84,4 +90,164 @@ fn part1(input: &str) {
     println!("{}", result);
 }
 
-fn part2(input: &str) {}
+fn dfs2(
+    grid: &[Vec<char>],
+    visited: &mut HashSet<(isize, isize)>,
+    new_visited: &mut HashSet<(isize, isize)>,
+    cur_area: &mut isize,
+    current: (isize, isize),
+) {
+    // println!("Visiting {:#?}", current);
+    visited.insert(current);
+    new_visited.insert(current);
+    *cur_area += 1;
+
+    for (dx, dy) in MAT {
+        let new_x = current.0 + dx;
+        let new_y = current.1 + dy;
+
+        if new_x < 0 || new_x >= grid.len() as isize || new_y < 0 || new_y >= grid[0].len() as isize
+        {
+            continue;
+        }
+
+        let (new_x, new_y) = (new_x as usize, new_y as usize);
+
+        if grid[new_x][new_y] != grid[current.0 as usize][current.1 as usize] {
+            continue;
+        }
+
+        if visited.contains(&(new_x as isize, new_y as isize)) {
+            continue;
+        }
+
+        dfs2(
+            grid,
+            visited,
+            new_visited,
+            cur_area,
+            (new_x as isize, new_y as isize),
+        );
+    }
+}
+
+fn count_neighbors(grid: &[Vec<char>], current: (isize, isize)) -> Vec<(isize, isize)> {
+    let mut result = Vec::new();
+
+    for (dx, dy) in MAT {
+        let new_x = current.0 + dx;
+        let var_name = current.1;
+        let new_y = var_name + dy;
+
+        if new_x < 0 || new_x >= grid.len() as isize || new_y < 0 || new_y >= grid[0].len() as isize
+        {
+            result.push((dx, dy));
+            continue;
+        }
+
+        if grid[current.0 as usize][current.1 as usize] != grid[new_x as usize][new_x as usize] {
+            result.push((dx, dy));
+        }
+    }
+
+    result
+}
+
+fn border_count(grid: &[Vec<char>], visited: &HashSet<(isize, isize)>) -> isize {
+    let mut border_count = 0;
+
+    let border = visited
+        .iter()
+        .flat_map(|&point| {
+            count_neighbors(grid, point)
+                .iter()
+                .map(|(dx, dy)| (point.0 + dx, point.1 + dy, *dx, *dy))
+                .collect::<Vec<_>>()
+        })
+        .collect::<HashSet<_>>();
+
+    println!("{:#?}", border.clone());
+
+    let near_map = visited
+        .iter()
+        .map(|&point| (point, count_neighbors(grid, point)))
+        .collect::<HashMap<(isize, isize), _>>();
+
+    let mut visited = HashSet::new();
+    for border_element in border.iter() {
+        let mut queue = VecDeque::new();
+
+        if visited.contains(border_element) {
+            continue;
+        }
+        border_count += 1;
+
+        queue.push_back(*border_element);
+
+        while let Some(top_elem) = queue.pop_back() {
+            println!("{:#?}", top_elem);
+            visited.insert(top_elem);
+            let (x, y, dx_old, dy_old) = top_elem;
+
+            for (dx, dy) in MAT {
+                let new_x = x + dx;
+                let new_y = y + dy;
+
+                for direction in near_map.get(&(new_x, new_y)).unwrap_or(&Vec::new()) {
+                    if *direction != (dx_old, dy_old) {
+                        continue;
+                    }
+
+                    println!("{} {} {} {}", new_x, new_y, direction.0, direction.1);
+
+                    if visited.contains(&(new_x, new_y, direction.0, direction.1)) {
+                        continue;
+                    }
+
+                    if !border.contains(&(new_x, new_y, direction.0, direction.1)) {
+                        continue;
+                    }
+
+                    queue.push_back((new_x, new_y, direction.0, direction.1));
+                }
+            }
+        }
+    }
+
+    border_count
+}
+
+fn part2(input: &str) {
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut visited = HashSet::new();
+    let mut result = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if visited.contains(&(i as isize, j as isize)) {
+                continue;
+            }
+
+            let mut cur_area = 0;
+            let mut new_visited = HashSet::new();
+
+            dfs2(
+                &grid,
+                &mut visited,
+                &mut new_visited,
+                &mut cur_area,
+                (i as isize, j as isize),
+            );
+
+            let border_count = border_count(&grid, &new_visited);
+            println!("{:#?}", border_count);
+            // panic!();
+        }
+    }
+
+    println!("{}", result);
+}
