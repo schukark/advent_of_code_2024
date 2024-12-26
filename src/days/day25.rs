3d6929d632df21ab48932d9465143a9133a85a42
schukark <schukark2004@gmail.com>
2024-12-26 13:27:52 +0300

    added: (infra) added automatic input file and day.rs file generation, added all days upto 25

---
+++ src/days/day25.rs
@@ -0,0 +1,19 @@
+use crate::utils::input::read_file;
+
+pub(crate) fn run(part: u32) {
+    let input = read_file("inputs/day25.txt");
+
+    match part {
+        1 => part1(&input),
+        2 => part2(&input),
+        _ => println!("Invalid part"),
+    }
+}
+
+fn part1(input: &str) {
+    
+}
+
+fn part2(input: &str) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,80 @@
-use std::{env, time::Instant};
+use std::error::Error;
+use std::io::prelude::*;
+use std::{env, fs, time::Instant};
+extern crate reqwest;
+use dotenv::dotenv;
+use reqwest::header;
+
 mod days;
 mod utils;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 3 && args[1] == "create" {
+        let day = args[2].parse::<u32>().unwrap();
+        fs::write(format!("src/days/day{}.rs", day), get_template(day))
+            .expect("Unable to write to file");
+
+        let mut mod_file = fs::OpenOptions::new()
+            .append(true)
+            .open("src/days/mod.rs")
+            .unwrap();
+
+        if let Err(e) = writeln!(mod_file, "pub mod day{};", day) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            header::COOKIE,
+            [
+                "session=",
+                &env::var("AOC_SESSION").unwrap_or("".to_string()),
+            ]
+            .concat()
+            .parse()
+            .unwrap(),
+        );
+
+        let client = reqwest::blocking::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap();
+        let res = client
+            .get(
+                [
+                    "https://adventofcode.com/2024/day/",
+                    &day.to_string(),
+                    "/input",
+                ]
+                .concat(),
+            )
+            .headers(headers)
+            .send()?
+            .text()?;
+
+        fs::write(format!("inputs/day{}.txt", day), res).expect("Unable to write to file");
+
+        return Ok(());
+    }
+
     if args.len() != 3 {
-        println!("Usage: cargo run <day> <part>");
-        return;
+        return Err("Usage: cargo run <day> <part>".into());
     }
 
     let day: u32 = match args[1].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Invalid day");
-            return;
+            return Err("Invalid day".into());
         }
     };
 
     let part: u32 = match args[2].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Invalid part");
-            return;
+            return Err("Invalid part".into());
         }
     };
 
@@ -44,8 +96,45 @@ fn main() {
         13 => days::day13::run(part),
         14 => days::day14::run(part),
         15 => days::day15::run(part),
+        16 => days::day16::run(part),
+        17 => days::day17::run(part),
+        18 => days::day18::run(part),
+        19 => days::day19::run(part),
+        20 => days::day20::run(part),
+        21 => days::day21::run(part),
+        22 => days::day22::run(part),
+        23 => days::day23::run(part),
+        24 => days::day24::run(part),
+        25 => days::day25::run(part),
         _ => println!("Invalid day"),
     }
 
     println!("Execution time: {:.2?}", before.elapsed());
+
+    Ok(())
+}
+
+fn get_template(day: u32) -> String {
+    format!(
+        "use crate::utils::input::read_file;
+
+pub(crate) fn run(part: u32) {{
+    let input = read_file(\"inputs/day{}.txt\");
+
+    match part {{
+        1 => part1(&input),
+        2 => part2(&input),
+        _ => println!(\"Invalid part\"),
+    }}
+}}
+
+fn part1(input: &str) {{
+    
+}}
+
+fn part2(input: &str) {{
+
+}}",
+        day
+    )
 }
